c6c1345f97de5c466b8f9175a2c9de30b3abf33d
donnydamanik <89127709+donnydamanik@users.noreply.github.com>
2022-03-01 20:06:07 +0700

    Create Modul1-No2.cpp

---
+++ Modul1-No2.cpp
@@ -0,0 +1,28 @@
+ #include <iostream>
+ #include <iomanip>
+ using namespace std;
+
+ int main()
+ {
+     int kode[5]={001, 002, 003, 004, 005};
+     string nama [5]={"Penghapus", "Pensil", "Buku", "Rautan", "Penggaris"};
+        int jumlah [5] = {4,3,2,3,5};
+     int harga [5]= {1000,1500,2000,1000,500};
+     int total [5];
+     int jumlah_item=0;
+     int jumlah_total=0;
+     cout << "=================================" << endl;
+ 	 cout << "Ahmad Donny Damanik / 21091397008" << endl;
+  	 cout << "================================= \n" << endl;
+     cout<<"Kode\tNama\t\tJumlah\tHarga\tTotal"<<endl;
+     for (int i=0; i<5; i++)
+     {
+         total[i]= jumlah[i]*harga[i];
+         cout<< setfill ('0') << setw (3)<< kode[i] << "\t" << nama[i] << "    \t" 
+		 << jumlah[i] << " \t" << harga[i] << "\t" << total[i] << endl;
+         jumlah_item= jumlah_item+ jumlah [i];
+         jumlah_total= jumlah_total+ jumlah [i];
+     }
+         cout<< "jumlah item = 17\n";
+         cout<< "Total pembelian = 18000\n";
+ }
